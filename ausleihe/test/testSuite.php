a1625325c4d4632b01b703b67f4376e9da51bf86
root <root@jupiter>
2021-06-07 20:43:22 +0200

    Change eclipse projet php type

---
+++ ausleihe/test/testSuite.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Static test suite.
+ */
+class testSuite extends PHPUnit_Framework_TestSuite
+{
+
+    /**
+     * Constructs the test suite handler.
+     */
+    public function __construct()
+    {
+        $this->setName('testSuite');
+    }
+
+    /**
+     * Creates the suite.
+     */
+    public static function suite()
+    {
+        return new self();
+    }
+}
+
