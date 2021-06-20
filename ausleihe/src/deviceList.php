b38870b5d44dc0665a405d5c503d8f0a253d8f6b
root <root@jupiter>
2021-06-20 11:20:49 +0200

    Aufgaben vorbereitet

---
+++ ausleihe/src/deviceList.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Devices</title>
+</head>
+<frameset>
+    <frame>
+    <frame>
+    <noframes>
+    <body>
+    <!-- Eine Uebersicht ueber alle Devices, ausgeliehene und nicht ausgeliehene getrennt in Tabelle, mit Links auf die 
+    einzelnen Geraete -->
+    <p>This page uses frames. The current browser you are using does not support frames.</p>
+    <?php
+
+	?>
+    </body>
+    </noframes>
+</frameset>
+</html>
